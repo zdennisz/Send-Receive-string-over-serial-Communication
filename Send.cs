a93ccbc199865c9451e5b1b4eb98722d0605fd01
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2019-09-17 18:41:18 +0300

    Serial Communication via Virtual Serial Port Sriver
    Send and Receive strings by converting them to byte array and send them and catching them and converting them back to a string

---
+++ Send.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SendDataOverCom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SerialPort s1=new SerialPort();
+            string strTosend= "Shir";
+
+            byte[] arrayTosend = Encoding.ASCII.GetBytes(strTosend);
+            s1.PortName = "COM3";
+            s1.Parity = Parity.None;
+            s1.BaudRate = 2400;
+            s1.DataBits = 8;
+            s1.StopBits =StopBits.One;
+            s1.Handshake = Handshake.None;
+
+            while (true)
+            {
+                try
+            {
+
+                if (!s1.IsOpen)
+                {
+                    s1.Open();
+                }
+                    s1.Write(arrayTosend, 0, arrayTosend.Length);
+                    Console.WriteLine("Sending Data...");
+                    Thread.Sleep(2500);
+                } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                Console.ReadKey();
+                s1.Close();
+            }
+                
+            }
+        }
+    }
+}
